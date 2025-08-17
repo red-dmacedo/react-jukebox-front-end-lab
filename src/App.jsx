a2a6d59966b5c4a1b2541cfd1ec9aef018ef5c70
dmacedo <devinm3333@outlook.com>
2025-08-17 09:00:12 -0700

    handle functions added

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,59 @@ const App = () => {
       }
     };
   }, []);
+
+  const selectTrack = (track) => {
+    setSelected(track);
+  };
+
+  const handleFormView = (track) => {
+    if (!track._id) setSelected(null);
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddTrack = async (formData) => {
+    try {
+      const newTrack = await trackService.create(formData);
+
+      if (newTrack.err) {
+        throw new Error(newTrack.err);
+      };
+
+      setTracks([newTrack, ...pets]);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdateTrack = async (formData, id) => {
+    try {
+      const updatedTrack = await trackService.update(formData, id);
+      if (updatedTrack.err) { throw new Error(updatedTrack.err) };
+      setTracks(tracks.map(track => { return (track._id !== updatedTrack._id) ? track : updatedTrack }));
+      setSelected(updatedTrack);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleRemoveTrack = async (id) => {
+    try {
+      const statusCode = await trackService.remove(id);
+      if (!statusCode.toString().match(/2\d{2}/)) {
+        throw new Error(`Status code was not a 200-299 value: ${statusCode}`);
+      };
+      setTracks(tracks.filter(track => track._id !== id));
+      setSelected(null);
+    } catch (err) {
+      console.log(err);
+    };
+  };
+
   return (
     <>
-    <h1>Hello!2</h1>
+      <h1>Hello!2</h1>
     </>
   );
 };
