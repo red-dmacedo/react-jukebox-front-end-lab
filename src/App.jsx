0745fb8e8ede9b6f3f5199ce2e5af22e3f008259
dmacedo <devinm3333@outlook.com>
2025-08-17 08:09:26 -0700

    render hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import * as trackService from '../services/trackService.js';
 
 
 const App = () => {
+  const [tracks, setTracks] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const fetchedTracks = await trackService.index();
+        if (fetchedTracks.err) { throw new Error(fetchTracks.err) };
+        setTracks(fetchedTracks);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }, []);
   return (
     <>
+    <h1>Hello!2</h1>
     </>
   );
 };
